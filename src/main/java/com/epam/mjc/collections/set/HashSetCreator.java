36ae9e7fbd82945492975393a5770b8c6cb706a0
Shushoook <shushoook666@gmail.com>
2023-02-09 22:47:33 +0400

    stage1-module4-collections-task2-set

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int x : sourceList) {
+            if (x % 2 == 0) {
+                while (x % 2 == 0) {
+                    hashSet.add(x);
+                    x /= 2;
+                }
+                hashSet.add(x);
+            } else {
+                hashSet.add(x);
+                hashSet.add(2 * x);
+            }
+        }
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,13 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> collect = sourceList.stream().map(x -> x * x).collect(Collectors.toCollection(TreeSet::new));
+        return collect.subSet(lowerBound, true, upperBound, true);
     }
 }
+
